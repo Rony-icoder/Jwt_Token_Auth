de3403ccf0e2f2cde92470ea71b051085f9d20d9
Rony-icoder <143245764+Rony-icoder@users.noreply.github.com>
2024-12-28 12:02:33 +0530

    Created user ans api

--- JWT_Auth/api/urls.py
+++ JWT_Auth/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-#nothing added data
+from django.contrib import admin
+from django.urls import path, include
+from .views import StudentView
+
 urlpatterns = [
-    
+    path("student", StudentView.as_view()),
 ]
+
+
